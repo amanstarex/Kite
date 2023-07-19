257e09c38da68f132be65e039b114d72b09cdd31
amanm <amanm@Aman>
2023-07-19 15:58:58 +0530

    kite commit

---
+++ kite/src/test/java/com/kite/kite/KiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kite.kite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
