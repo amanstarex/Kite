6ed66ea26fe05ba4cd4b3de60db608cbd42a211e
amanm <amanm@Aman>
2023-07-19 18:51:15 +0530

    change#3

--- kite/src/main/java/com/kite/KiteApplication.java
+++ kite/src/main/java/com/kite/KiteApplication.java
@@ -1,4 +1,4 @@
-package com.kite.Starter;
+package com.kite;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- kite/src/main/java/com/kite/Service/TradeService.java
+++ kite/src/main/java/com/kite/Service/TradeService.java
@@ -1,9 +1,5 @@
 package com.kite.Service;
 
-
-
-
-
 public class TradeService {
 	
 	
